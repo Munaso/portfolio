1cb4370596cff400c0828e0692afaec978d0bf22
Munaso <109114487+Munaso@users.noreply.github.com>
2023-03-12 11:02:30 +0900

    fixed bugs of scrolling buttons

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 // Make navbar transparent when it is on the top
 let navbar = document.getElementById('navbar');
 let navbarHeight = navbar.getBoundingClientRect().height;
-console.log('navbarHeight:'+ navbarHeight)
 document.addEventListener("scroll",()=>{
+
   if(window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   }  
@@ -18,6 +18,8 @@ document.addEventListener("scroll",()=>{
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu')
 navbarMenu.addEventListener('click', (event)=>{
+  const target = event.target;
+  selectNavItem(target)
   const link = event.target.dataset.link;
   if(link == null){
     return;
@@ -32,6 +34,71 @@ navbarMenu.addEventListener('click', (event)=>{
     behavior: 'smooth'
   });
 })
+
+// show menu border when scrolling
+
+// 1. 모든 섹션 요소들을 가지고온다.
+// 2. IntersectinObserver 를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이테을 활성화시킨다.
+
+const sectionIds = [
+  '#home',
+  '#about',
+  '#work',
+  '#testimonials',
+  '#contact',
+];
+
+const sections = sectionIds.map(id => document.querySelector(id))
+
+const navItems = sectionIds.map(id => 
+  document.querySelector(`[data-link="${id}"]`))
+
+let selectedNavIndex = 0
+let selectedNavItem = navItems[0];
+
+function selectNavItem(selected)  {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+  root : null,
+  rootMargin: '-100px',
+  threshold: 0.3,
+}
+
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting && entry.intersectionRatio > 0){
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if(entry.boundingClientRect.y < 0){
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  })
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', ()=>{
+  if(window.scrollY === 0){
+    selectedNavIndex = 0;
+  } else if(
+    window.scrollY + window.innerHeight === 
+    document.body.clientHeight
+    ) {
+      selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+})
+
+
 
 // Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
@@ -51,7 +118,10 @@ function scrollIntoView(selector){
     top:top,
     behavior: 'smooth'
   });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
+  console.log(sectionIds.indexOf(selector))
 }
+
 const contactMeBtn = document.querySelector('.home__contact')
 contactMeBtn.addEventListener('click', ()=>{
   scrollIntoView('#contact');
@@ -76,9 +146,8 @@ document.addEventListener('scroll', ()=>{
 
 // handle click on the "arrow up" button
 arrowUp.addEventListener('click', ()=>{
-  console.log('11')
   scrollIntoView('#home');
-  
+
 })
 
 // organize project list with buttons
@@ -98,7 +167,6 @@ work__categories.addEventListener('click', (event)=>{
   selected.classList.remove('selected')
   event.target.classList.add('selected')
 
-  console.log(filter)
 
   projectContainer.classList.add('anim-out');  
   setTimeout(() => {
